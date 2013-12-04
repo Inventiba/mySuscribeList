253b1fd0d1d2013d1a777fa3314cb529b2c8e569
Mathieu Lemaire <contact@getvega.com>
2013-12-04 16:37:37 +0100

    Database Operations

--- models/subscriptors.js
+++ models/subscriptors.js
@@ -9,50 +9,50 @@ var subscriber = {};
 
 subscriber.db = new dataBase (name, new server(host, port, {auto_reconnect: true},{}));
 subscriber.db.open(function(e,d) {
-	if(e) {
-		console.log(e)
-	} else {
-		console.log('Connected to database: ' + name);
-	}
+    if(e) {
+        console.log(e)
+    } else {
+        console.log('Connected to database: ' + name);
+    }
 });
 
 subscriber.subscribers = subscriber.db.collection('subscribers');
 
 module.exports = subscriber;
 
-subscriber.new = function Insert(newData, callback) {
-	subscriber.subscribers.findOne({email: newData.email}, function(e,obj) {
-		if(obj) {
-			callback('Email already exist.');
-		} else {
-			subscriber.subscribers.insert(newData, callback(null))
-		}
-	})
+subscriber.new = function Insert(newData, callBack) {
+    subscriber.subscribers.findOne({email: newData.email}, function(e,obj) {
+        if(obj) {
+            callBack('Email already exist.');
+        } else {
+            subscriber.subscribers.insert(newData, callBack(null))
+        }
+    })
 }
 
-subscriber.list = function Show(callback) {
-	subscriber.subscribers.find().toArray(function(e,res) {
-		if(e) {
-			callback(e)
-		} else {
-			callback(null, res)
-		}
-	})
+subscriber.list = function Show(callBack) {
+    subscriber.subscribers.find().toArray(function(e,res) {
+        if(e) {
+            callBack(e)
+        } else {
+            callBack(null, res)
+        }
+    })
 }
 
 subscriber.edit = function Update(newData, callback) {
-	subscriber.subscribers.findOne({_id: this.getObjectId(newData.id)}, function(e,o) {
-		o.name = newData.name;
-		o.email = newData.email;
-		subscriber.subscribers.save(o);
-		callback(o);
-	})
+    subscriber.subscribers.findOne({_id: this.getObjectId(newData.id)}, function(e,o) {
+        o.name = newData.name;
+        o.email = newData.email;
+        subscriber.subscribers.save(o);
+        callback(o);
+    })
 }
 
 subscriber.delete = function Erase(id, callback) {
-	subscriber.subscribers.remove({_id: this.getObjectId(id)},callback)
+    subscriber.subscribers.remove({_id: this.getObjectId(id)},callback)
 }
 
 subscriber.getObjectId = function GetId(id) {
-	return subscriber.subscribers.db.bson_serializer.ObjectID.createFromHexString(id)
+    return subscriber.subscribers.db.bson_serializer.ObjectID.createFromHexString(id)
 }
