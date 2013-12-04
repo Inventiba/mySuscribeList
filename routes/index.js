e454cbed1aa68e4cb56c2618ae7951550b0424ab
Mathieu Lemaire <contact@getvega.com>
2013-12-04 16:07:24 +0100

    routes drive

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ var subscriberModule = require('../models/subscriptors');
 module.exports = function(app) {
 
     app.get('/', function(request, response) {
-	    subscriberModule.list(function(e, subs) {
+	    subscriberModule.list(function(error, subs) {
 		    response.render('index', { title: 'Subscriber List',error:'', subscriptors: subs });
 		})
 	})
 
 	app.post('/', function(request, response) {
-	   	subscriberModule.new({name: request.param('name'), email: request.param('email')}, function(o){
-			subscriberModule.list(function(e, subs) {
-				response.render('index', { title: 'Subscriber List', error:o, subscriptors: subs });
+	   	subscriberModule.new({name: request.param('name'), email: request.param('email')}, function(e) {
+			subscriberModule.list(function(error, subs) {
+				response.render('index', { title: 'Subscriber List', error:e, subscriptors: subs });
 			})
 		})
 	})
 
 	app.post('/save', function(request,response) {
 		var string= {name: request.param('name'), email:request.param('email'), id:request.param('id')};
-		subscriberModule.edit(string, function(o){
-			if(o) {
+		subscriberModule.edit(string, function(e) {
+			if(e) {
 				response.redirect('/');
 			} else {
 				response.send('update field fail',400)
@@ -28,8 +28,8 @@ module.exports = function(app) {
 	})
 
 	app.post('/delete', function(request, response) {
-		subscriberModule.delete(request.body.id, function(e,obj) {
-			if(!e) {
+		subscriberModule.delete(request.body.id, function(error,obj) {
+			if(!error) {
 				response.send('ok',200)
 			} else {
 				response.send('subscriber does no exist', 400)
